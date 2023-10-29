2db9990d8827231c9bb7d240797fd427ae744ab2
sijan <sijanshrestha2@gmail.com>
2023-10-29 18:19:25 +0700

    fix: import

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from './ui/home.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 
-import {lusitana} from "@/app/ui/font";
+import {lusitana} from "@/app/ui/fonts";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
